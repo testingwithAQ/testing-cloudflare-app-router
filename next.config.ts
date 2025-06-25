d2001fe8d19623ac385dcb12ef7c0dcafa3526f6
testingwithAQ <testingwithaq1O1@gmail.com>
2025-06-25 13:05:57 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,8 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  // output: 'export', // ✅ Enables static export
+};
+
+export default nextConfig;
